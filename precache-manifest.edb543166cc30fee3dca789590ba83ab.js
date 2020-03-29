680de0507e6a89903ed02c552c772c272a238e21
Litt <andrew.litt@queensu.ca>
2020-03-29 11:08:19 -0400

    Updates

---
+++ precache-manifest.edb543166cc30fee3dca789590ba83ab.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe017c7248e61cd8adfd7cb87af41053",
+    "url": "/react-nhl/index.html"
+  },
+  {
+    "revision": "f1babc95b86b9612016e",
+    "url": "/react-nhl/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "4d93675d3fb46fce657d",
+    "url": "/react-nhl/static/css/main.c1457f1d.chunk.css"
+  },
+  {
+    "revision": "f1babc95b86b9612016e",
+    "url": "/react-nhl/static/js/2.a9500bfa.chunk.js"
+  },
+  {
+    "revision": "e7df5ec6c9986765193824c7f118c284",
+    "url": "/react-nhl/static/js/2.a9500bfa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4d93675d3fb46fce657d",
+    "url": "/react-nhl/static/js/main.8b8d1880.chunk.js"
+  },
+  {
+    "revision": "8f5b07e5b08a909fc05f",
+    "url": "/react-nhl/static/js/runtime-main.3b43b3ae.js"
+  },
+  {
+    "revision": "2ac4215f0756be144108c5af278a0551",
+    "url": "/react-nhl/static/media/FuturaBQ-DemiBold.2ac4215f.otf"
+  },
+  {
+    "revision": "11f68b85b54bc53a7344b1a9bc814435",
+    "url": "/react-nhl/static/media/FuturaLT.11f68b85.ttf"
+  },
+  {
+    "revision": "e6920dd747441cd6021d8e38f35e463e",
+    "url": "/react-nhl/static/media/circle.e6920dd7.png"
+  },
+  {
+    "revision": "9090a187dfadef136c8d9a0c5acdaffe",
+    "url": "/react-nhl/static/media/diamond.9090a187.png"
+  },
+  {
+    "revision": "a268fdd702a21db8cbb7f9c7423e9a9e",
+    "url": "/react-nhl/static/media/rinkBW.a268fdd7.png"
+  },
+  {
+    "revision": "007cf8d216175230f54dc69f936fb0cd",
+    "url": "/react-nhl/static/media/square.007cf8d2.png"
+  },
+  {
+    "revision": "7ad38c591cf5789f186aca00db22cd5b",
+    "url": "/react-nhl/static/media/triangle.7ad38c59.png"
+  },
+  {
+    "revision": "afd12ac2ec447a69a58d5fd2015cb347",
+    "url": "/react-nhl/static/media/wye.afd12ac2.png"
+  }
+]);
